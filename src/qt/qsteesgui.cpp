ca51e1cce2003030361f793528c86d9f182d7565
stevecat <60027963+stevecat@users.noreply.github.com>
2020-01-24 10:15:46 -0500

    cleanup

--- src/qt/qsteesgui.cpp
+++ src/qt/qsteesgui.cpp
@@ -2,7 +2,7 @@
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#include <qt/singui.h>
+#include <qt/qsteesgui.h>
 
 #include <qt/qsteesunits.h>
 #include <qt/clientmodel.h>
